4af2392ec6a99d0c9aab4d1c007b0dffd6d768c1
khuslens <khuslen.cyber@gmail.com>
2022-07-26 01:37:37 +0000

    s3 bucket name added as an output in S3 child module

--- session-10/main.tf
+++ session-10/main.tf
@@ -10,6 +10,7 @@ module "ec2" {
   ami           = "ami-098e42ae54c764c35"
   instance_type = "t2.micro"
   env           = "dev"
+  s3_bucket_name = module.s3.id
 }
 
 # Using Terraform Registry as a remote module
