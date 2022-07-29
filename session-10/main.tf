7920b7370a2c43b4f1de494115618dd537d90e56
Khuslentuguldur <khuslens@Khuslentuguldurs-MacBook-Pro-2.local>
2022-07-29 16:51:46 +0900

    updated:

--- session-10/main.tf
+++ session-10/main.tf
@@ -12,38 +12,38 @@ module "ec2" {
   env           = "dev"
   s3_bucket_name = module.s3.bucket_name
 }
-# module.s3.bucket_name = module.module_name.output_name
+ module.s3.bucket_name = module.module_name.output_name
 
-# Module only reference to an output, this is only way to reference to other modules
+ Module only reference to an output, this is only way to reference to other modules
 
-# Using Terraform Registry as a remote module
-# module "vpc" {
-#   source = "terraform-aws-modules/vpc/aws"
-#   cidr = "10.0.0.0/16"
+ Using Terraform Registry as a remote module
+ module "vpc" {
+   source = "terraform-aws-modules/vpc/aws"
+   cidr = "10.0.0.0/16"
 
-#   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-#   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-#   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-#   enable_nat_gateway = true
-#   enable_vpn_gateway = true
+   enable_nat_gateway = true
+   enable_vpn_gateway = true
 
-#   tags = {
-#     Terraform = "true"
-#     Environment = "dev"
-#   }
-# }
-
-
+   tags = {
+     Terraform = "true"
+     Environment = "dev"
+   }
+ }
 
 
-# github.com/aKumoSolutions/terraform-june-2022/tree/main/modules/s3
 
 
-# github.com = domain name
-# aKumoSolutions = Organization
-# terraform-june-2022 = Repository
-# tree = Branches
-# main = Branch
-# modules = Subdirectory
-# s3 = Subdirectory
+//  github.com/aKumoSolutions/terraform-june-2022/tree/main/modules/s3
+
+
+//  github.com = domain name
+// # aKumoSolutions = Organization
+// # terraform-june-2022 = Repository
+// # tree = Branches
+// # main = Branch
+// # modules = Subdirectory
+// # s3 = Subdirectory
